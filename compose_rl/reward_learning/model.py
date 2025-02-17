Fixing gradients

compose_rl/reward_learning/model.py
@@ -9,7 +9,7 @@ from functools import partial
 from typing import Any, Mapping, MutableMapping, Optional, Union
 
 import torch
-from composer.utils import is_model_fsdp
+from composer.utils import dist, is_model_fsdp
 from llmfoundry.models import ComposerHFCausalLM, ComposerMPTCausalLM
 
 from compose_rl.reward_learning.base_reward import RewardModel, Tokenizer
@@ -216,9 +216,13 @@ class ComposerHFCausalRewardModel(ComposerHFCausalLM, RewardModel):
         self.reset_output_embed = False
         self.should_reset_output_embed = should_reset_output_embed
 
+        # if kwargs['init_device'] == 'mixed':
+        # if dist.get_global_rank() == 0:
+        # self.mask_last_embed_except_eos()
+
     def mask_last_embed_except_eos(
         self,
-        fill_value: float = float('-inf'),
+        fill_value: float = -100,
     ) -> None:
         """Mask out all but the last embedding for the EOS token."""
         print('model is: ', self.model)
@@ -238,8 +242,8 @@ class ComposerHFCausalRewardModel(ComposerHFCausalLM, RewardModel):
             mask = torch.full_like(self.model.lm_head.weight.data, fill_value)
 
             # mask[self.eos_token_id, :] = self.model.lm_head.weight.data[
-                # self.eos_token_id, :]
-            
+            # self.eos_token_id, :]
+
             # Reset the values here? It might help
             mask[self.eos_token_id, :] = 0.0
 
@@ -251,7 +255,7 @@ class ComposerHFCausalRewardModel(ComposerHFCausalLM, RewardModel):
             print(self.model.lm_head.weight.data[self.eos_token_id, :])
 
         self.reset_output_embed = True
-        logging.info("Finished resetting output embedding layer.')")
+        logging.info('Finished resetting output embedding layer.')
 
     def forward(
         self,
@@ -262,7 +266,7 @@ class ComposerHFCausalRewardModel(ComposerHFCausalLM, RewardModel):
 
         is_inference = batch.get('is_inference', False)
         if is_inference:
-            # Inference code should be able to
+            # Inference code should be able to handle this arbitrary result
             logits = self.model(
                 batch['input_ids'],
                 attention_mask=batch['attention_mask'],

compose_rl/reward_learning/model_methods.py
@@ -291,42 +291,6 @@ def causal_rm_forward(
         model_output = model(
             batch_cat_inputs,
             attention_mask=batch_attn_mask,
-        )  # If we can't use attn_seq_id then we need to unpack each batch and
-        # Pack along the batch dimension instead.
-
-        chosen_inputs, rejected_inputs = extract_packed_chosen_rejected(
-            input_tensor=batch['input_ids'],
-            chosen_len=batch['chosen_len'],
-            rejected_len=batch['rejected_len'],
-            max_seq_len=concat_seq_len // 2,
-            pad_token_id=pad_token_id,
-        )
-
-        chosen_attention_mask, rejected_attention_mask = extract_packed_chosen_rejected(
-            input_tensor=batch['text_attention_mask'],
-            chosen_len=batch['chosen_len'],
-            rejected_len=batch['rejected_len'],
-            max_seq_len=concat_seq_len // 2,
-            pad_token_id=0,
-        )
-
-        batch_cat_inputs = torch.cat([chosen_inputs, rejected_inputs], dim=0)
-        batch_attn_mask = torch.cat(
-            [
-                chosen_attention_mask,
-                rejected_attention_mask,
-            ],
-            dim=0,
-        )
-
-        # Dynamic Padding
-        max_length = max(max(batch['chosen_len']), max(batch['rejected_len']))
-        batch_cat_inputs = batch_cat_inputs[:, :max_length]
-        batch_attn_mask = batch_attn_mask[:, :max_length]
-
-        model_output = model(
-            batch_cat_inputs,
-            attention_mask=batch_attn_mask,
         )
 
         # We consider the scores the logits from the model
@@ -352,6 +316,9 @@ def causal_rm_forward(
         dim=1,
         index=batch['rejected_len'].view(-1, 1) - 1,
     )
+
+    print('chosen scores are: ', chosen_scores)
+    print('rejected scores are: ', rejected_scores)
 
     outputs = {
         'chosen_scores': chosen_scores,