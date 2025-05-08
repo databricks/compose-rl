598579772e59b39c62f6c7e966af6129fdea2abe
brandon <bcui8377@gmail.com>
2025-05-08 18:49:55 +0000

    update

--- compose_rl/utils/vllm_actor.py
+++ compose_rl/utils/vllm_actor.py
@@ -80,6 +80,19 @@ class LLMRayActor:
             *args,
             **kwargs,
         )
+    
+    def chat(self, *args: Any, **kwargs: Any):
+        log.info(f'Chat kwargs are: {kwargs}')
+        sampling_params = None
+        if 'sampling_params' in kwargs:
+            sampling_params = SamplingParams(**kwargs.pop('sampling_params'))
+            log.info(f'sampling_params is: {sampling_params}')
+
+        return self.llm.chat(
+            *args,
+            **kwargs,
+            sampling_params=sampling_params,
+        )
 
     def init_process_group(
         self,
