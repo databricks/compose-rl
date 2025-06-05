9b3a9630ec6f237251cbb9064ea4ee5133a46997
Jonathan Chang <j.chang@databricks.com>
2025-06-05 14:37:42 -0400

    style fix

--- compose_rl/dpo/model_methods.py
+++ compose_rl/dpo/model_methods.py
@@ -1,2 +1,7 @@
-from compose_rl.algorithms.offline.model_methods import PairwiseOfflineEnum as DPOEnum
-from compose_rl.algorithms.offline.model_methods import pairwise_offline_loss as dpo_loss
+# Copyright 2024 MosaicML ComposeRL authors
+# SPDX-License-Identifier: Apache-2.0
+
+from compose_rl.algorithms.offline.model_methods import \
+    PairwiseOfflineEnum as DPOEnum  # pyright: ignore
+from compose_rl.algorithms.offline.model_methods import \
+    pairwise_offline_loss as dpo_loss  # pyright: ignore
