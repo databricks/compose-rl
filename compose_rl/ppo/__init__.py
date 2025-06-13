a86d5aff5de93eaa2518fb5da44ccb158109830d
Jonathan Chang <j.chang@databricks.com>
2025-06-13 17:46:02 -0400

    orl backwards compat

--- compose_rl/ppo/__init__.py
+++ compose_rl/ppo/__init__.py
@@ -1,2 +1,5 @@
+# Copyright 2024 MosaicML ComposeRL authors
+# SPDX-License-Identifier: Apache-2.0
+
 from compose_rl.algorithms.online.callback import \
     OnPolicyCallback as PPOCallback  # pyright: ignore
