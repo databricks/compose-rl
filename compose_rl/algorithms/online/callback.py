2de97d27abdd03a567f4e2baac0343adf963db3e
Jonathan Chang <j.chang@databricks.com>
2025-06-16 12:10:34 -0400

    add enum to generation utils

--- compose_rl/algorithms/online/callback.py
+++ compose_rl/algorithms/online/callback.py
@@ -1117,7 +1117,8 @@ class OnPolicyCallback(CallbackWithConfig):
             self.vllm_engines,
             self.model_update_group,
             batch,
-            loss_type=self.actor_critic.loss_type.value,  # type: ignore
+            #loss_type=self.actor_critic.loss_type.value,  # type: ignore
+            loss_type=self.actor_critic.loss_type,  # type: ignore
         )
         log.info('Finished broadcasting to vLLM')
         log.info(f'Took: {time.time() - start_time} to broadcast to vllm.')
