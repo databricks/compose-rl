779774673fe819ada1961b6b679e20d650e9bd22
Jonathan Chang <j.chang@databricks.com>
2025-06-23 15:49:55 -0400

    remove dirs

--- compose_rl/dpo/model_methods.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2024 MosaicML ComposeRL authors
-# SPDX-License-Identifier: Apache-2.0
-
-from compose_rl.algorithms.offline.model_methods import \
-    PairwiseOfflineEnum as DPOEnum  # pyright: ignore
-from compose_rl.algorithms.offline.model_methods import \
-    pairwise_offline_loss as dpo_loss  # pyright: ignore
