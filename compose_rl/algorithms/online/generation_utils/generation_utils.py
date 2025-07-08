594f77df7067a6a5b4ad6236b70af699f5f97d78
Jonathan Chang <j.chang@databricks.com>
2025-07-08 09:05:35 -0400

    precommit

--- compose_rl/algorithms/online/generation_utils/generation_utils.py
+++ compose_rl/algorithms/online/generation_utils/generation_utils.py
@@ -302,8 +302,8 @@ def vllm_generate(
 
     if vllm_generate_function == 'chat':
         all_messages = [
-            message for batch in all_batched_messages for message_list in batch  # type: ignore
-            for message in message_list
+            message for batch in all_batched_messages  # type: ignore
+            for message_list in batch for message in message_list
         ]
         assert len(all_prompts) == len(all_messages)
 
