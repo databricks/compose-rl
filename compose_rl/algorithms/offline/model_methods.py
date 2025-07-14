batch advantage computation fix

compose_rl/algorithms/offline/model_methods.py
@@ -178,7 +178,7 @@ def offline_loss(
     if loss_type == RegressionOfflineEnum.APO:
         loss_dict['estimated_reward'] = estimated_reward
         loss_dict['batch_advantage'] = torch.mean(
-            batch['reward'] - batch['vstar'],
+            batch['reward'] - vstar,
         )
 
     if 'lbl' in outputs: