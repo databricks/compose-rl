fixed under/overflow bug with sigmoid

compose_rl/algorithms/reward_modeling/functional.py
@@ -10,6 +10,7 @@ import math
 from typing import MutableMapping
 
 from pydantic import BaseModel
+import numpy as np
 import torch
 
 log = logging.getLogger(__name__)
@@ -607,6 +608,14 @@ class JudgementScoreVerifierReward(BaseVerifierReward):
             label = bool(label)
         reward_scalar = self._mse_reward(answer, label)
         return self.reward * reward_scalar
+    
+    def _safe_sigmoid(self, x, clip_min=-250, clip_max=250):
+        if x > clip_max:
+            return 1.0
+        if x < clip_min:
+            return 0.0
+        return 1.0 / (1.0 + np.exp(-x))
+
 
     def _mse_reward(self, generated_score: float, target: bool) -> float:
         """
@@ -624,5 +633,5 @@ class JudgementScoreVerifierReward(BaseVerifierReward):
         """
         assert isinstance(target, bool), f'Target must be a boolean, got {type(target)}'
         target = 1.0 if target else 0.0
-        generated_score = 1.0 / (1.0 + math.exp(-generated_score))
+        generated_score = self._safe_sigmoid(generated_score)
         return 1.0 - (generated_score - target) ** 2