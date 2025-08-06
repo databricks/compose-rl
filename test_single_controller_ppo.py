5d872e05ce7739552b84a85531f8207528e1351e
bowenyang008 <byang008@ucr.edu>
2025-08-06 06:46:02 +0000

    8K

--- test_single_controller_ppo.py
+++ test_single_controller_ppo.py
@@ -51,10 +51,10 @@ from compose_rl.algorithms.online.callback_utils import preprocess_batches
 GLOBAL_TRAIN_BATCH_SIZE = 64
 GENERATIONS_PER_PROMPT = 8  
 NUM_BATCHES_PER_UPDATE = 8
-NUM_TRAIN_ITERATIONS = 5
+NUM_TRAIN_ITERATIONS = 10
 
-_MAX_SEQ_LEN = 6000
-_MAX_GEN_LEN = 4000
+_MAX_SEQ_LEN = 8000
+_MAX_GEN_LEN = 8000
 
 
 @contextmanager
