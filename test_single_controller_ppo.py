01666bf7bbfb55f311dc1a6e7616855d9d34bc89
bowenyang008 <byang008@ucr.edu>
2025-08-06 21:17:56 +0000

    update try setting worker node

--- test_single_controller_ppo.py
+++ test_single_controller_ppo.py
@@ -460,7 +460,7 @@ class InferenceServer:
                 device_bundle={
                     'GPU': 1,
                     'CPU': 1,
-                    'worker_node': 0,
+                    'worker_node': 1,
                 },
             )
 
