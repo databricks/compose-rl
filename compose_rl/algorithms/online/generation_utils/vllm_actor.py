4ed2322d20dd7a988efcff14075a2a8d456fb15f
Ethan Tang <ethan.tang@databricks.com>
2025-08-09 11:08:45 -0700

    assert false

--- compose_rl/algorithms/online/generation_utils/vllm_actor.py
+++ compose_rl/algorithms/online/generation_utils/vllm_actor.py
@@ -124,6 +124,7 @@ class LLMRayActor:
         shape: Union[tuple[int, ...], list[int]],
         empty_cache: bool = False,
     ):
+        assert False, "weight update"
         log.info(f'Updating weight {name} with shape {shape} and dtype {dtype}')
         return self.llm.collective_rpc(
             'update_weight',
