3e0628f0fc0e678df946e186dc392bf2c6b76ca5
bowenyang008 <byang008@ucr.edu>
2025-08-22 00:36:18 +0000

    uv magic

--- test_run_sglang_server.sh
+++ test_run_sglang_server.sh
@@ -6,8 +6,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # install sglang
 uv pip install "sglang[all]" --system
-# our version of vllms does not work with transformers 4.54.0
-# uv pip install "transformers<4.54.0"
 
 # manually run sglang server and test
 # python -m sglang.launch_server --model-path qwen/qwen2.5-0.5b-instruct
