0476ea6dcbcf52a4aac2b08bd8956256ecbe308c
bowenyang008 <byang008@ucr.edu>
2025-08-22 00:32:10 +0000

    use uv again

--- test_run_sglang_server.sh
+++ test_run_sglang_server.sh
@@ -1,10 +1,13 @@
 # install numactl
 apt-get update && apt-get install -y numactl
 
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 # install sglang
-pip install "sglang[all]"
+uv pip install "sglang[all]" --system
 # our version of vllms does not work with transformers 4.54.0
-pip install "transformers<4.54.0"
+# uv pip install "transformers<4.54.0"
 
 # manually run sglang server and test
 # python -m sglang.launch_server --model-path qwen/qwen2.5-0.5b-instruct
