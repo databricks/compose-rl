1e9d123db175dbf9955f5836009a15d65a6f7b49
wensun <ws455@cornell.edu>
2025-08-27 14:14:59 -0400

    recreating wrong example beta string

--- compose_rl/algorithms/online/model_methods.py
+++ compose_rl/algorithms/online/model_methods.py
@@ -437,7 +437,8 @@ def policy_loss(
             dim=-1,
         )  #size: (batch_size,)
         # Convert beta to a simple float
-        beta_float = float(beta)
+        # TODO: fix that later!! this is for debugging.
+        beta_float = beta #float(beta)
         policy_loss = ((beta_float * masked_log_probs_diff - prompt_advantages)**2).mean()       
 
         rewards = utils.masked_sum(
