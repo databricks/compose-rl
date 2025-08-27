b5f5da1a45e484e8f9b47e68cc9b4f9cebbe6d6b
wensun <ws455@cornell.edu>
2025-08-27 16:12:53 -0400

    convert back to the correct version and check in the yaml for smd

--- compose_rl/algorithms/online/model_methods.py
+++ compose_rl/algorithms/online/model_methods.py
@@ -448,8 +448,7 @@ def policy_loss(
         
         policy_dict = {
             'loss/policy_loss': policy_loss,
-            #'kl/policy_kl': policy_kl,  # Required by calling code in model.py
-            'kl/ref_policy_kl': policy_kl,
+            'kl/policy_kl': policy_kl,  # Required by calling code in model.py
             'kl/old_policy_kl': old_policy_kl,
             'gen/gen_length': batch['action_mask'].sum(dim=1).to(torch.float32),
             'gen/entropy': old_entropies,
