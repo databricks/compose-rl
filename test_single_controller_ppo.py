e5f81641a4f9c5e911928202f8aa878b03fcbaed
wensun <ws455@cornell.edu>
2025-08-31 23:33:26 -0400

    .

--- test_single_controller_ppo.py
+++ test_single_controller_ppo.py
@@ -28,6 +28,7 @@ from multiprocessing.pool import AsyncResult, Pool
 
 from composer.loggers import MLFlowLogger
 import ray
+ray.init(_internal_config={"logging_level": "DEBUG"})
 import spacy
 import torch
 import torch.distributed as dist
