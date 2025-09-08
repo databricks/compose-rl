5e2316a6f0b274e103b5affc844a37e713bac213
Owen Oertell <owen.oertell@databricks.com>
2025-09-08 15:01:07 -0400

    Add params.yaml

--- compose_rl/algorithms/offline/model_methods.py
+++ compose_rl/algorithms/offline/model_methods.py
@@ -128,7 +128,7 @@ def offline_forward(
 
     outputs: dict[str, torch.Tensor] = {
         'policy_logp': logps,
-        'policy_logits': output_logits,
+        'policy_logits': output_logits[:,:-1],
         'sequence_entropies': sequence_entropies,
     }
 
@@ -209,6 +209,13 @@ def offline_loss(
         assert batch['reward'] is not None, "reward must be in the batch. called from offline_loss fn"
 
         losses = (policy_logits[:, :, 0] - batch['reward']) ** 2
+        
+        # print all the shapes
+        print(f"losses shape: {losses.shape}")
+        print(f"policy_logits[:, :, 0] shape: {policy_logits[:, :, 0].shape}")
+        print(f"batch['mask'][:,1:] shape: {batch['mask'][:,1:].shape}")
+        print(f"batch['attention_mask'][:,1:] shape: {batch['attention_mask'][:,1:].shape}")
+
         
         losses *= batch['mask'][:,1:] 
         losses *= batch['attention_mask'][:,1:]
