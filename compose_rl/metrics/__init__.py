c8fa666198de8c3fb1510dfc93fb98e61835e1fd
Owen Oertell <owen.oertell@databricks.com>
2025-09-09 15:42:56 -0400

    .

--- compose_rl/metrics/__init__.py
+++ compose_rl/metrics/__init__.py
@@ -3,10 +3,7 @@
 
 from compose_rl.metrics.reward_model_metrics import \
     PairwiseRewardClassificationAccuracy
-from compose_rl.metrics.learning_metrics import \
-    TestLossMetric
 
 __all__ = [
     'PairwiseRewardClassificationAccuracy',
-    'TestLossMetric',
 ]
